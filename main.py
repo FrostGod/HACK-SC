b0fd7463df42542e1d9c2d31b338baa4c722094d
allenyuchen <allenyuchen@hotmail.com>
2023-11-05 00:20:51 -0700

    add mock data

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import yt_dlp
 import webvtt
 from yt_dlp import YoutubeDL
+from yt_dlp.utils import DownloadError
 import pandas as pd
 
 app = FastAPI()
@@ -12,17 +13,6 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"Data: Test"}
-
-
-@app.post("/upload")
-async def upload_video(url: str):
-    return {"Data": "hello"}
-
-
-@app.post("/get_subtitles")
-async def get_subtitles(url: str):
-    data = extract_subtitles(url)
-    return {"Data": data}
 
 
 def get_video_metadata(ydl: YoutubeDL, url: str):
@@ -62,6 +52,7 @@ def extract_video_data(url: str):
         "outtmpl": tmp_file_str,
         "writeautomaticsub": True,
     }
+    vtt_file_str = ""
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             video_metadata = get_video_metadata(ydl, url)
@@ -78,35 +69,77 @@ def extract_video_data(url: str):
             "description": video_description,
             "subtitles": subtitle_dict,
         }
+    except DownloadError:
+        st.error("Invalid Youtube URL")
     finally:
         if os.path.exists(vtt_file_str):
             os.remove(vtt_file_str)
 
 
-def draw_subtitles(subtitle_dict):
-    subtitles = subtitle_dict["subtitles"]
-    title = subtitle_dict["title"]
+def draw_subtitles(video_data: dict):
+    subtitles = video_data["subtitles"]
+    title = video_data["title"]
     st.write(title)
     if st.toggle("Show Description"):
-        st.write(subtitle_dict["description"])
+        st.write(video_data["description"])
     df = pd.DataFrame(subtitles, columns=["start", "end", "caption_text"])
     st.dataframe(df)
-    st.video(subtitle_dict["url"])
+    st.video(video_data["url"])
 
+MOCK_VIDEO_DATA = {
+    "url": "https://www.youtube.com/watch?v=OkmNXy7er84",
+}
+MOCK_DATA = [
+    ["Introduction to the Putnam competition", 0, 13, "An introduction to the Putnam competition, a challenging math competition for undergraduate students with 12 questions."],
+    ["The Challenging Problem", 72, 96, "The main problem discussed in the video is introduced - 'If you choose four random points on a sphere and consider the tetrahedron with these points as its vertices, what is the probability that the center of the sphere is inside that tetrahedron?'"],
+    ["Two-Dimensional Case", 114, 135, "Simplifying the problem by considering a two-dimensional case where three random points are chosen on a circle."],
+    ["Probability in Two Dimensions", 179, 224, "Explaining the probability calculation in two dimensions, where the average probability that the triangle contains the center of the circle is determined."],
+    ["Extension to Three Dimensions", 266, 283, "Extending the problem to three dimensions and explaining how to calculate the probability for the tetrahedron containing the center of the sphere."],
+    ["Elegant Insight", 345, 383, "Discussing the elegant insight of reframing the problem and thinking about choosing lines and points instead of random points."],
+    ["General Problem-Solving Approach", 391, 435, "Providing a general problem-solving approach by breaking down complex problems into simpler cases and looking for key insights."],
+    ["Sponsor Message - Brilliant.org", 575, 621, "Promoting Brilliant.org as a platform for enhancing problem-solving skills and presenting a probability puzzle related to cheating students."],
+    ["Conclusion", 668, 671, "Concluding remarks and closing the video."]
+]
+
+MOCK_COLUMNS = ['Title', 'Start', 'End', 'Text']
+MOCK_DF = pd.DataFrame(MOCK_DATA, columns=MOCK_COLUMNS)
+
+def extract_video_id(url: str):
+    return url.split("=")[-1]
+
+def draw_notes(notes_df: pd.DataFrame, video_data: dict):
+    for i, row in notes_df.iterrows():
+        if i != 0:
+            st.divider()
+        col1, col2 = st.columns([2, 1])
+        with col1:
+            st.subheader(row['Title'])
+            st.write(row['Text'])
+        with col2:
+            video_id = extract_video_id(video_data["url"])
+            st.video(video_data["url"], start_time=row['Start'])
+
+def header():
+    col1, col2, col3 = st.columns([1, 2, 1])
+    with col2:
+        st.title("Generate Notes from Youtube Videos")
+        if "video_data" not in st.session_state:
+            st.session_state.video_data = {}
+        url = st.text_input(
+            "Enter the youtube video url",
+            placeholder="https://www.youtube.com/watch?v=OkmNXy7er84",
+        )
+        if st.button("Generate!"):
+            st.session_state.video_data = extract_video_data(url)
 
 def streamlit_app():
-    st.title("Generate Notes from Youtube Videos")
-    if "video_data" not in st.session_state:
-        st.session_state.video_data = {}
-    url = st.text_input(
-        "Enter the youtube video url",
-        placeholder="https://www.youtube.com/watch?v=OkmNXy7er84",
-    )
-    if st.button("Generate!"):
-        st.session_state.video_data = extract_video_data(url)
+    st.set_page_config(layout="wide")
+    header()
     if st.session_state.video_data:
-        subtitle_dict = st.session_state.data
-        draw_subtitles(subtitle_dict)
+        video_data = st.session_state.video_data
+        # draw_subtitles(video_data)
+        print(video_data)
+        draw_notes(MOCK_DF, MOCK_VIDEO_DATA)
 
 
 if __name__ == "__main__":
